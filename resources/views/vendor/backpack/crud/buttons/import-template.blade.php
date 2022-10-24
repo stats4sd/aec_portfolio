f03be90226f5eb2a84e7ff3cb14fff6a854ed4e0
Dave Mills <d.e.mills@stats4sd.org>
2022-10-24 13:01:57 +0100

    fix typos

--- resources/views/vendor/backpack/crud/buttons/import-template.blade.php
+++ resources/views/vendor/backpack/crud/buttons/import-template.blade.php
@@ -1,3 +1,3 @@
 @if ($crud->hasAccess('update'))
-	<a href="{{ url($crud->route.'/import-template') }}" class="btn btn-link" data-button-type="import-template"><i class="la la-download"></i> Downlaod Template for Imports</a>
+	<a href="{{ url($crud->route.'/import-template') }}" class="btn btn-link" data-button-type="import-template"><i class="la la-download"></i> Download Template for Imports</a>
 @endif
