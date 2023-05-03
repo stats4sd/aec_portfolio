8a397f36a1a03bd62a4670e1f4e84715db991807
Dan Tang <dan@stats4sd.org>
2023-05-03 16:26:31 +0100

    Project CRUD, list view, Assess Project button, enable or disable it by referring to latest assessment instead of project

--- resources/views/vendor/backpack/crud/buttons/assess.blade.php
+++ resources/views/vendor/backpack/crud/buttons/assess.blade.php
@@ -1,8 +1,7 @@
 @if ($crud->hasAccess('update'))
-    <!-- TODO: check overall score and assessment status of latest assessment instead of project -->
-    <!-- Question: it shows Javascript source code in "Assess Project" button location after referring to latest assessment -->
+    <!-- DONE - TODO: check overall score and assessment status of latest assessment instead of project -->
 	<a
-        @if($entry->overall_score === 0 || $entry->assessment_status === \App\Enums\AssessmentStatus::NotStarted || $entry->assessment_status === \App\Enums\AssessmentStatus::RedlinesIncomplete)
+    @if($entry->assessments->last()->overall_score === 0 || $entry->assessments->last()->assessment_status === \App\Enums\AssessmentStatus::NotStarted || $entry->assessments->last()->assessment_status === \App\Enums\AssessmentStatus::RedlinesIncomplete)
             class="btn btn-info disabled"
         @else
             href="{{ url($crud->route.'/'.$entry->id.'/assess') }}"
