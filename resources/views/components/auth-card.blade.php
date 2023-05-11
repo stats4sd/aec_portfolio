962c79e2320a751628fa074a63937a3d9adaa430
Dave Mills <d.e.mills@stats4sd.org>
2023-05-11 12:52:50 +0100

    login page design draft

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,10 +1,13 @@
-<div class="d-flex flex-sm-column justify-content-center pt-6">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="d-flex flex-column justify-content-center pt-6">
 
-    <div class="card shadow-sm bg-white rounded">
-        <div class="card-body">
+    <div class="card shadow-lg bg-white rounded p-5 border-light">
+        <div class="mt-3">
+            {{ $logo }}
+        </div>
+
+        <div class="card-body my-4">
+            <h3 class="text-center">Portfolio Assessment Tool</h3>
+            <h4 class="text-center">LOGIN</h4>
             {{ $slot }}
         </div>
     </div>
