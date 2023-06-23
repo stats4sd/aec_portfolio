4b01c70f23813b470090031ebcab3ec7442daf7e
Dave Mills <d.e.mills@stats4sd.org>
2023-06-23 22:15:49 +0300

    add org currency; xe helper placeholder

---
+++ app/Services/XeCurrencyHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class XeCurrencyHelper
+{
+
+    public static function convert(int $budget, string $currencyFrom, string $currencyTo): int
+    {
+        // TODO: add call to XE Currency Converter
+        return $budget;
+    }
+
+}
