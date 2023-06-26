1ed0ab7c7d386b882104323ce1ddbafb8e328da5
Dave Mills <d.e.mills@stats4sd.org>
2023-06-26 18:12:48 +0100

    fix typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        @vite('resources/css/app.scss')
+        @vite('resources/sass/app.scss')
 
         <!-- Scripts -->
         @vite('resources/js/app.js')
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    @vite('resources/css/app.scss')
+    @vite('resources/sass/app.scss')
 
     <style type="text/css" media="screen">
         html,
