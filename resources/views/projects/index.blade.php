57827f4d8391303af1c4e6b22f90fa5d36b9b5d2
Dave Mills <d.e.mills@stats4sd.org>
2023-06-26 17:30:41 +0100

    add search of initiatives list

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
     <div class="container-fluid mt-16" id="initiativesListPage">
-        <h1 class="font-weight-bold text-deep-green mb-4">{{ $organisation->name }} - Initiatives </h1>
-         <initiatives-list
-                :initiatives="{{ $projects->toJson() }}"
-                :has-additional-assessment="{{ $has_additional_assessment ? 'true' : 'false' }}"
-            />
+        <initiatives-list
+            :organisation="{{ $organisation }}"
+            :initiatives="{{ $projects->toJson() }}"
+            :has-additional-assessment="{{ $has_additional_assessment ? 'true' : 'false' }}"
+        />
     </div>
 
 @endsection
