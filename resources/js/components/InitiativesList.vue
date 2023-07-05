00fed5bacec989e23ff03f534f5bee6ead94e426
Dan Tang <dan@stats4sd.org>
2023-07-05 15:53:22 +0100

    1. Revise codes for filtering records, make sorting works when no filter applied 2. Add sortBy, sortDir in computed() function, make sorting works when any filter applied 3. Complete resetFilters() function, make Reset button works 4. Sort initiatives records by name in ascending order, which match the default value of sortBy and sortDir

--- resources/js/components/InitiativesList.vue
+++ resources/js/components/InitiativesList.vue
@@ -113,6 +113,7 @@ const sortOptions = ref([
     }
 ])
 
+// default value for sortBy and sortDir, which is sorted by name in ascending order
 const sortBy = ref('name')
 const sortDir = ref(1)
 
@@ -123,8 +124,8 @@ const propComparator = (propName, sortDir) =>
 watch(sortDir, (newSortDir) => {
     props.initiatives.sort(propComparator(sortBy.value, newSortDir))
 })
+
 watch(sortBy, (newSortBy) => {
-    console.log('hi');
     props.initiatives.sort(propComparator(newSortBy, sortDir.value))
 })
 
@@ -133,6 +134,9 @@ watch(sortBy, (newSortBy) => {
 const redlineStatusFilter = ref('');
 const principleStatusFilter = ref('');
 const portfolioFilter = ref('');
+
+// keyword search filter
+const searchString = ref('')
 
 const portfolios = computed(() => {
     return props.initiatives.map(initiative => initiative.portfolio.name)
@@ -174,38 +178,50 @@ function makeFilterOptions(string) {
 }
 
 const filteredInitiatives = computed(() => {
-    return props.initiatives.filter(initiative => {
-        if (redlineStatusFilter.value) {
-            return initiative.latest_assessment.redline_status === redlineStatusFilter.value.value
-        }
+    // just mentioning variables, computed() function will be triggered when their value changed
+    sortBy.value;
+    sortDir.value;
 
-        return true;
-    })
-        .filter(initiative => {
-            if (principleStatusFilter.value) {
-                return initiative.latest_assessment.principle_status === principleStatusFilter.value.value
-            }
+    // declare temporary variables for filtering
+    let tempInitiatives;
+    tempInitiatives = props.initiatives;
 
-            return true;
-        })
-        .filter(initiative => {
-            if (portfolioFilter.value) {
-                return initiative.portfolio.name === portfolioFilter.value
-            }
+    // apply filter for red flag status
+    if (redlineStatusFilter.value) {
+        tempInitiatives = tempInitiatives.filter(
+            initiative => initiative.latest_assessment.redline_status === redlineStatusFilter.value.value
+        )
+    }
 
-            return true;
-        })
-        .filter(initiative => {
+    // apply filter for principle status
+    if (principleStatusFilter.value) {
+        tempInitiatives = tempInitiatives.filter(
+            initiative => initiative.latest_assessment.principle_status === principleStatusFilter.value.value
+        )
+    }
 
-            if (searchString.value !== '') {
-                return initiative.name.toLowerCase().includes(searchString.value.toLowerCase())
-            }
+    // apply filter for portfolio
+    if (portfolioFilter.value) {
+        tempInitiatives = tempInitiatives.filter(
+            initiative => initiative.portfolio.name === portfolioFilter.value
+        )
+    }
 
-            return true
-        })
+    // apply filter for keyword
+    if (searchString.value !== '') {
+        tempInitiatives = tempInitiatives.filter(
+            initiative => initiative.name.toLowerCase().includes(searchString.value.toLowerCase())
+        )
+    }
+
+    return tempInitiatives;
 })
 
 function resetFilters() {
+    redlineStatusFilter.value = '';
+    principleStatusFilter.value = '';
+    portfolioFilter.value = '';
+    searchString.value = '';
 }
 
 // handle portfolio from url
@@ -222,9 +238,6 @@ onMounted(() => {
     })
 
 })
-
-//  search
-const searchString = ref('')
 
 
 </script>
