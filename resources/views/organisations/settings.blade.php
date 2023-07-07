3c4aaaee33955403f00f37df8fa2357da2381e27
Dan Tang <dan@stats4sd.org>
2023-07-07 17:12:46 +0100

    My Institution > Settings > Add access right control to Discard Changes button, Save button

--- resources/views/organisations/settings.blade.php
+++ resources/views/organisations/settings.blade.php
@@ -56,8 +56,8 @@
 
 
         <div class="form-group d-flex justify-content-end mt-16">
-            <button type="cancel" class="btn btn-secondary mr-4">Discard Changes</button>
-            <button type="submit" class="btn btn-primary">Save</button>
+            <button type="cancel" class="btn btn-secondary mr-4" {{ Auth::user()->can('edit own institution') ? '' : 'disabled' }}>Discard Changes</button>
+            <button type="submit" class="btn btn-primary {{ Auth::user()->can('edit own institution') ? '' : 'disabled' }}">Save</button>
         </div>
     </form>
 </div>
