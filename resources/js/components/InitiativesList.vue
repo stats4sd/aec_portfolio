347fa96c5af5a5e9f9fc4eb6c6e5061b2770c0b3
Dave Mills <d.e.mills@stats4sd.org>
2023-07-24 11:41:22 +0100

    re-add updates from previous PR

--- resources/js/components/InitiativesList.vue
+++ resources/js/components/InitiativesList.vue
@@ -79,7 +79,14 @@
         </div>
     </div>
 
-    <InitiativeListCard v-for="initiative in filteredInitiatives" :key="initiative.id" :initiative="initiative" :has-additional-assessment="hasAdditionalAssessment" :enable-edit-button="enableEditButton" :enable-show-button="enableShowButton" :enable-assess-button="enableAssessButton"
+    <InitiativeListCard
+        v-for="initiative in filteredInitiatives"
+        :key="initiative.id"
+        :initiative="initiative"
+        :has-additional-assessment="hasAdditionalAssessment"
+        :enable-edit-button="enableEditButton"
+        :enable-show-button="enableShowButton"
+        :enable-assess-button="enableAssessButton"
     />
 
 </template>
@@ -94,7 +101,7 @@ import InitiativeListCard from "./InitiativeListCard.vue";
 
 const props = defineProps({
     organisation: Object,
-    initiatives: Object,
+    initialInitiatives: Object,
     hasAdditionalAssessment: Boolean,
     showAddButton: Boolean,
     showImportButton: Boolean,
@@ -129,11 +136,11 @@ const propComparator = (propName, sortDir) =>
     (a, b) => a[propName] === b[propName] ? 0 : a[propName] < b[propName] ? -sortDir : sortDir
 
 watch(sortDir, (newSortDir) => {
-    props.initiatives.sort(propComparator(sortBy.value, newSortDir))
+    initiatives.value.sort(propComparator(sortBy.value, newSortDir))
 })
 
 watch(sortBy, (newSortBy) => {
-    props.initiatives.sort(propComparator(newSortBy, sortDir.value))
+    initiatives.value.sort(propComparator(newSortBy, sortDir.value))
 })
 
 
@@ -149,7 +156,7 @@ const searchString = ref('')
 let flagInitialised = 0;
 
 const portfolios = computed(() => {
-    return props.initiatives.map(initiative => initiative.portfolio.name)
+    return initiatives.value.map(initiative => initiative.portfolio.name)
         .reduce((cumulative, current) => {
             if (cumulative.includes(current)) {
                 return cumulative;
@@ -197,7 +204,7 @@ const filteredInitiatives = computed(() => {
 
     // declare temporary variables for filtering
     let tempInitiatives;
-    tempInitiatives = props.initiatives;
+    tempInitiatives = initiatives.value;
 
     // apply filter for red flag status
     if (redlineStatusFilter.value) {
@@ -318,7 +325,19 @@ function storeLatestSettings() {
     }
 }
 
+function removeInitiative(initiative) {
+    let tempArray = initiatives.value.map(initiative => initiative.id);
+
+    let index = tempArray.indexOf(initiative.id);
+
+    initiatives.value.splice(index, 1);
+}
+
+const initiatives = ref([])
+
 onMounted(() => {
+
+    initiatives.value = [...props.initialInitiatives]
     handleSettingsFromUrl();
 })
 
