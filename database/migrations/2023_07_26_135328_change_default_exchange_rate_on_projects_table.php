cde4519d4893efc3fa15206c1c8e6ae7727bd958
Dave Mills <d.e.mills@stats4sd.org>
2023-07-27 12:44:59 +0100

    set null exchange rates to 1 before db structure change

--- database/migrations/2023_07_26_135328_change_default_exchange_rate_on_projects_table.php
+++ database/migrations/2023_07_26_135328_change_default_exchange_rate_on_projects_table.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        // in case db has null values for exchange_rate:
+        Project::withoutGlobalScopes(['organisation'])
+            ->where('exchange_rate', '=', null)
+            ->update(['exchange_rate' => 1.0]);
+
+
         Schema::table('projects', function (Blueprint $table) {
-            $table->decimal('exchange_rate')->nullable(false)->default(1)->change();
+            $table->decimal('exchange_rate', 10, 6)->nullable(false)->default(1)->change();
         });
     }
 
