680dddb4609617e324e24b7e90beff1336ba3d01
Dan Tang <dan@stats4sd.org>
2025-01-16 14:09:33 +0000

    add middleware to middleware groups in Kernel.php, to avoid returning error 403

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,12 +38,14 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            TeamsPermission::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+            TeamsPermission::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
