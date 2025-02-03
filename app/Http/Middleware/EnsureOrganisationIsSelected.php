9dcf94cf3e826274fb9c73374f4e01d398f4c1cc
Dave Mills <d.e.mills@stats4sd.org>
2025-02-03 15:23:03 +0000

    Redirect user to requested url instead of hard-coded home page.

--- app/Http/Middleware/EnsureOrganisationIsSelected.php
+++ app/Http/Middleware/EnsureOrganisationIsSelected.php
@@ -21,11 +21,11 @@ class EnsureOrganisationIsSelected
         if (Organisation::count() === 1) {
             Session::put('selectedOrganisationId', Organisation::first()->id);
 
-            // tried to call setPermissionsTeamId() here, but problem persists.
-            // redirect user to My Institution page helps to load user's role, problem resolved.
-            return redirect(url('admin/organisation/show'));
+            // redirect to restart the entire request / middleware stack, so that the initial attempt to set the team for role/permission assignment can be done
+            return redirect($request->url());
         }
 
+        // For users with multiple institutions redirect them to select an organisation.
         Session::flash('redirect', $request->fullUrl());
 
         return redirect(backpack_url('selected_organisation'));
