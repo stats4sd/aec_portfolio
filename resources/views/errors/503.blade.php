ceaeefb4bd47dda7360c77b31fc6ada686ecb993
Dave Mills <d.e.mills@stats4sd.org>
2025-06-10 10:37:20 +0100

    make the maintenance mode message friendlier

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -5,12 +5,12 @@
 @endphp
 
 @section('title')
-  It's not you, it's me.
+ Updates in progress
 @endsection
 
 @section('description')
   @php
     $default_error_message = "This site is temporarily offline while we deploy updates and run some maintenance checks. Please try again in a few minutes.";
   @endphp
-  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
+  {!! $default_error_message !!}
 @endsection
